1ddcc54da10c65faaa6082aa38ff8c3aa62a307e
Maueen-Njango <maureennjangombira@gmail.com>
2024-03-31 18:21:31 +0300

    add files

---
+++ stacks.py
@@ -0,0 +1,13 @@
+#creating a stack
+stack = []
+stack.append("Mangoes")
+stack.append("Banana")
+stack.append("Apples")
+stack.append("Guava")
+print(stack)
+#accessing
+print(stack[0])
+ #removing
+print(stack.pop())
+
+ 
